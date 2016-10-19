c20a41a5f4d39dd2fa9fd8c05543284494d6bfe7
Anushri HV <anushri.veerarajan@gmail.com>
2016-10-19 22:46:23 +0530

    second commit

--- test.js
+++ test.js
@@ -1,24 +1,24 @@
-function multiply(){
-a=Number(document.my_cal.first.value);
-b=Number(document.my_cal.second.value);
-c=a*b;
-document.my_cal.total.value=c;
-}
-function addition(){
-a=Number(document.my_cal.first.value);
-b=Number(document.my_cal.second.value);
-c=a+b;
-document.my_cal.total.value=c;
-}
-function division(){
-a=Number(document.my_cal.first.value);
-b=Number(document.my_cal.second.value);
-c=a/b;
-document.my_cal.total.value=c;
-}
-function subtraction(){
-a=Number(document.my_cal.first.value);
-b=Number(document.my_cal.second.value);
-c=a-b;
-document.my_cal.total.value=c;
-}
+var myVar=document.getElementById('display');
+
+myVar[0].onclick = function() { myVar[0].innerHTML= "Answer is " + 1;
+};
+
+var myAns = ""  ;
+
+
+var a = function (x){
+    myAns= myAns + x;
+    myVar[0].innerHTML=myAns;
+};
+
+var ac = function(){
+    myAns="";
+    myVar[0].innerHTML=myAns;
+};
+
+var ev=function(){
+   var resu;
+   resu = eval(myAns);
+    myVar[0].innerHTML=resu;
+    myAns="";
+};
